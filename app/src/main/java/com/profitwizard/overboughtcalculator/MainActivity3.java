0b7a9d19aacb330427080a1d40ff6d6deda24a58
DmitriyQW <varavkadmitrij@gmail.com>
2024-03-05 21:40:49 +0300

    Добавил на страницу с историей viewbanding,заблокировал ориентацию только портретная,удолил мусор.

--- app/src/main/java/com/profitwizard/overboughtcalculator/MainActivity3.java
+++ app/src/main/java/com/profitwizard/overboughtcalculator/MainActivity3.java
@@ -4,21 +4,20 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity3 extends AppCompatActivity {
+import com.profitwizard.overboughtcalculator.databinding.ActivityMain3Binding;
 
+public class MainActivity3 extends AppCompatActivity {
+    private ActivityMain3Binding bunding3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        bunding3 = ActivityMain3Binding.inflate(getLayoutInflater());
+        setContentView(bunding3.getRoot());
     }
 }
