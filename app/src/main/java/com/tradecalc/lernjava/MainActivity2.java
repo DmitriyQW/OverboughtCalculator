946ff34a30540cb348d0a897fae029926f7f67a3
DmitriyQW <varavkadmitrij@gmail.com>
2024-04-14 19:06:47 +0300

    Сделал подтягивание актуальных валют с цб и конвертацию в долар юань евро.

--- app/src/main/java/com/tradecalc/lernjava/MainActivity2.java
+++ app/src/main/java/com/tradecalc/lernjava/MainActivity2.java
@@ -28,6 +28,10 @@ public class MainActivity2 extends AppCompatActivity {
     private boolean onclicDolar = false;
     private boolean onclicYany = false;
 
+    private String evro_curse,dolar_curse,yuan_curse;
+    private float curse_evor,curse_dolar,curse_yan;
+
+    private String curse_dolar_format,curse_evor_format,curse_yan_format;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
@@ -35,7 +39,7 @@ public class MainActivity2 extends AppCompatActivity {
         bunding2 = ActivityMain2Binding.inflate(getLayoutInflater());
         setContentView(bunding2.getRoot());
 
-        init();
+
         bunding2.imageViewEvroBacgraund.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -81,22 +85,55 @@ public class MainActivity2 extends AppCompatActivity {
             if (onclicDolar  == false & onclicEvro == false & onclicYany == false){
                showInfo("Выберете валюту ");
             } else if (onclicDolar == true) {
-                Double rezconvertDolar = Float.parseFloat(bunding2.editTextTextInConvert.getText().toString()) / 92.45;
+                Double rezconvertDolar = (double) (Float.parseFloat(bunding2.editTextTextInConvert.getText().toString()) / curse_dolar);
                 String rezultDolar = String.format("%.2f",rezconvertDolar) + " $" ;
                 bunding2.textViewRezultCalcule.setText(rezultDolar);
             }else if (onclicEvro == true) {
-                Double rezconvertEvro = Float.parseFloat(bunding2.editTextTextInConvert.getText().toString()) / 99.85;
+                Double rezconvertEvro = (double) (Float.parseFloat(bunding2.editTextTextInConvert.getText().toString()) / curse_evor);
                 String rezultEvro = String.format("%.2f",rezconvertEvro) + " €" ;
                 bunding2.textViewRezultCalcule.setText(rezultEvro);
             }else if (onclicYany == true) {
-                Double rezconvertYany = Float.parseFloat(bunding2.editTextTextInConvert.getText().toString()) / 13.03;
+                Double rezconvertYany = (double) (Float.parseFloat(bunding2.editTextTextInConvert.getText().toString()) / curse_yan);
                 String rezultYany = String.format("%.2f",rezconvertYany) + " ¥";
                 bunding2.textViewRezultCalcule.setText(rezultYany);
             }
 
         }
     });
+bunding2.imageButtonActualValute.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        init();
+            bunding2.textViewDolar.setText(String.valueOf(curse_dolar_format));
+            bunding2.textViewEvro.setText(String.valueOf(curse_evor_format));
+            bunding2.textViewYany.setText(String.valueOf(curse_yan_format));
+            showInfo("Курс влюты обнавлён !");
 
+            if (dolar_curse != null & evro_curse != null & yuan_curse != null){
+
+                dolar_curse = dolar_curse.replace(",",".");
+                evro_curse = evro_curse.replace(",",".");
+                yuan_curse = yuan_curse.replace(",",".");
+
+                curse_dolar = Float.parseFloat(dolar_curse);
+                curse_evor = Float.parseFloat(evro_curse);
+                curse_yan = Float.parseFloat(yuan_curse);
+
+                curse_dolar_format = String.format("%.2f",curse_dolar);
+                curse_evor_format = String.format("%.2f",curse_evor);
+                curse_yan_format = String.format("%.2f",curse_yan);
+
+                curse_dolar = Float.parseFloat(dolar_curse);
+                curse_evor = Float.parseFloat(evro_curse);
+                curse_yan = Float.parseFloat(yuan_curse);
+            }
+//            if (doc != null){
+//                curse_dolar = Float.parseFloat(dolar_curse);
+//                curse_evor = Float.parseFloat(evro_curse);
+//                curse_yan = Float.parseFloat(yuan_curse);
+//            }
+    }
+});
 }
     public void showInfo(String text){
         Toast.makeText(this,text,Toast.LENGTH_SHORT).show();
@@ -115,13 +152,39 @@ public class MainActivity2 extends AppCompatActivity {
     private void getweb(){
         try {
             doc = Jsoup.connect("https://cbr.ru/currency_base/daily/").get();
+
             Elements tables = doc.getElementsByTag("tbody");
             Element select_table = tables.get(0);
             Elements elements_from_table = select_table.children();
-            Log.d("title","Зоголовок : "+doc.title());
-            Log.d("text",doc.text());
-            Log.d("size",String.valueOf(tables.get(0).text()));
-            Log.d("one_elm",String.valueOf(elements_from_table.get(1).text()));
+
+            //Долар
+            Element dolar = elements_from_table.get(14);
+            Elements dolar_children = dolar.children();
+
+            //Евро
+            Element evro = elements_from_table.get(15);
+            Elements evro_cheldren = evro.children();
+
+            //Юань
+            Element yan = elements_from_table.get(23);
+            Elements yan_cheldren = yan.children();
+
+            dolar_curse = String.valueOf(dolar_children.get(4).text());
+            evro_curse = String.valueOf(evro_cheldren.get(4).text());
+            yuan_curse = String.valueOf(yan_cheldren.get(4).text());
+
+
+//            Log.d("title","Зоголовок : "+doc.title());
+//            Log.d("text",doc.text());
+//            Log.d("size",String.valueOf(tables.get(0).text()));
+//            Log.d("one_elm",String.valueOf(elements_from_table.get(14).text()));
+
+//            Log.d("dolar[0]-nummber",String.valueOf(dolar_children.get(0).text()));
+//            Log.d("dolar[1]-neam-valute",String.valueOf(dolar_children.get(1).text()));
+//            Log.d("dolar[2]-count_item",String.valueOf(dolar_children.get(2).text()));
+//            Log.d("dolar[3]-ru_transteut",String.valueOf(dolar_children.get(3).text()));
+//            Log.d("dolar[4]-valute_curse",String.valueOf(dolar_children.get(4).text()));
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
